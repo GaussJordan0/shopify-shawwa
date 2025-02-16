35fbb5b3750f32e245d2eb389caef1bcf18c1383
GaussJordan0 <gaussjordanwork@gmail.com>
2025-02-16 17:10:06 +0200

    Publishing the repo

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-red-500 text-9xl font-bold flex items-center justify-center">
+      <div>Lets do this Nour</div>
+    </div>
+  );
+}
